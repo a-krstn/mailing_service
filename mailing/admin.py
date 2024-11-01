d25d75b760f448485c39fa2a7cd25cbfad2b9c0b
Alexandr <art1456@mail.ru>
2024-11-01 17:59:36 +0300

    created models: Mailing, Client, Message

--- mailing/admin.py
+++ mailing/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Mailing, Client, Message
+
+
+@admin.register(Mailing)
+class MailingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'text', 'start_time', 'end_time')
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('id', 'phone_number', 'operator_code', 'tag')
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'created_time', 'mailing', 'client')
--- mailing/models.py
+++ mailing/models.py
@@ -1,3 +1,103 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Mailing(models.Model):
+    """
+    Модель рассылки
+    """
+
+    text = models.TextField(
+        verbose_name='Текст рассылки'
+        )
+    start_time = models.DateTimeField(
+        verbose_name='Дата и время запуска рассылки'
+        )
+    end_time = models.DateTimeField(
+        verbose_name='Дата и время окончания рассылки'
+        )
+    filter = models.JSONField(
+        verbose_name='Фильтр свойств клиентов'
+        )
+    
+
+    class Meta:
+        verbose_name = 'Рассылка'
+        verbose_name_plural = 'Рассылки'
+
+
+    def __str__(self):
+        """
+        Строковое представление объекта рассылки
+        """
+
+        return f'Рассылка №{self.pk}'
+
+
+class Client(models.Model):
+    """
+    Модель клиента
+    """
+
+    phone_number = models.CharField(
+        max_length=11,
+        unique=True,
+        verbose_name='Номер телефона'
+        )
+    operator_code = models.CharField(
+        max_length=3,
+        verbose_name='Код мобильного оператора'
+        )
+    tag = models.CharField(
+        max_length=30,
+        verbose_name='Произвольная метка'
+        )
+
+
+    class Meta:
+        verbose_name = 'Клиент'
+        verbose_name_plural = 'Клиенты'
+
+
+    def __str__(self):
+        """
+        Строковое представление объекта клиента
+        """
+
+        return f'{self.pk}:{self.phone_number}'
+
+
+class Message(models.Model):
+    """
+    Модель сообщения
+    """
+
+    created_time = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Время создания'
+        )
+    mailing = models.ForeignKey(
+        Mailing,
+        on_delete=models.CASCADE,
+        verbose_name='Рассылка',
+        related_name='messages'
+        )
+    client = models.ForeignKey(
+        Client,
+        on_delete=models.CASCADE,
+        verbose_name='Клиент',
+        related_name='messages'
+        )
+    
+
+    class Meta:
+        verbose_name = 'Сообщение'
+        verbose_name_plural = 'Сообщения'
+
+    
+    def __str__(self):
+        """
+        Строковое представление модели сообщения
+        """
+
+        return f'Сообщение №{self.pk}'
